d7784f8d78746c1bf92c278ed92f85498287f91e
suryapratap-01 <suryapratap.singh2021@iem.edu.in>
2023-11-03 22:05:47 +0530

    Setting Up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './globals.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>
+        Hello Colors of Bharat !
+      </h1>
+    </div>
+  )
+}
+
+export default App
