357e0bb9bb6d875aad33e76ebda7a527920e3b69
suryapratap-01 <suryapratap.singh2021@iem.edu.in>
2023-11-09 23:37:09 +0530

    Home and Posts

--- src/root/pages/CreatePost.tsx
+++ src/root/pages/CreatePost.tsx
@@ -1,8 +1,20 @@
+import PostForm from "@/components/forms/PostForm"
 
 const CreatePost = () => {
   return (
-    <div>
-      
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="max-w-5xl flex-start gap-3 justify-start
+        w-full">
+          <img src="/assets/icons/add-post.svg" alt="add-post" 
+          width={36}
+          height={36}
+          />
+          <h2 className="h3-bold md:h2-bold test-left w-full"></h2>
+        </div>
+
+        <PostForm/>
+      </div>
     </div>
   )
 }
